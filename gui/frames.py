539376c80dcbf7ad85b5528f026147e2c74e6b4d
Robert Walker <rrah99@gmail.com>
2015-03-02 21:06:38 +0000

    Stays on top

--- gui/frames.py
+++ gui/frames.py
@@ -3,8 +3,10 @@ import wx
 import panels
 
 class Main_Window(wx.Frame):
-    def __init__(self, parent, title = 'Corio Control', *args, **kwargs):
+    def __init__(self, parent, title = 'Corio Control', style = wx.STAY_ON_TOP, *args, **kwargs):
         wx.Frame.__init__(self, parent, title = title, *args, **kwargs)
+        style = self.GetWindowStyle() | wx.STAY_ON_TOP
+        self.SetWindowStyle(style)
         
         panels.Main_Panel(self)
         
